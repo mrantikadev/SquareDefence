e6d4636a1615d297e8181a0710d33b9c74d8b7ad
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-05-02 11:07:13 +0300

    Testing

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake: Test");
+    }
+
+    private void Start()
+    {
+        Debug.Log("Start: Test");
+    }
+}
