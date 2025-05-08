51b116f9717d9723ef4760463987e899c04d2691
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-05-08 16:29:47 +0300

    Taşlar yerine oturrmaya başladı

--- Assets/Scripts/Turrets/SniperTurret.cs
+++ Assets/Scripts/Turrets/SniperTurret.cs
@@ -2,13 +2,9 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class SniperTurret : MonoBehaviour, ITurret
+public class SniperTurret : Turret
 {
-    public TurretSO type;
     private float fireCooldown;
-    private float currentHealth;
-    private Quaternion defaultRotation;
-    private List<BaseEnemy> enemiesInRange = new List<BaseEnemy>();
     private CircleCollider2D circleColllider;
 
     private void Awake()
@@ -28,11 +24,11 @@ public class SniperTurret : MonoBehaviour, ITurret
     {
         fireCooldown -= Time.deltaTime;
 
-        BaseEnemy closestEnemy = GetClosestEnemy();
+        BaseEnemy closestEnemy = base.GetClosestEnemy();
 
         if (closestEnemy != null)
         {
-            RotateTowards(closestEnemy.transform.position);
+            base.RotateTowards(closestEnemy.transform.position);
 
             if (fireCooldown <= 0)
             {
@@ -42,71 +38,9 @@ public class SniperTurret : MonoBehaviour, ITurret
         }
         else
         {
-            RotateBackToDefault();
+            base.RotateBackToDefault();
         }
     }
-
-    public void RotateTowards(Vector3 targetPosition)
-    {
-        Vector3 direction = targetPosition - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
-        Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
-
-        transform.rotation = Quaternion.RotateTowards(
-            transform.rotation,
-            targetRotation,
-            type.RotationSpeed * Time.deltaTime
-        );
-    }
-
-    public void RotateBackToDefault()
-    {
-        transform.rotation = Quaternion.RotateTowards(
-            transform.rotation,
-            defaultRotation,
-            type.RotationSpeed * Time.deltaTime
-        );
-    }
-
-    public void FireAt(BaseEnemy target)
-    {
-        GameObject projectile = Instantiate(type.ProjectilePrefab, transform.position, Quaternion.identity);
-        projectile.GetComponent<Projectile>().SetValues(type.BulletSpeed, type.BulletDamage);
-        projectile.GetComponent<Projectile>().Initialize(target);
-    }
-
-    public BaseEnemy GetClosestEnemy()
-    {
-        enemiesInRange = enemiesInRange.Where(enemy => enemy != null).ToList();
-
-        if (enemiesInRange.Count == 0) return null;
-
-        Debug.Log("Enemies in range: " + enemiesInRange.Count);
-        return enemiesInRange
-            .OrderBy(enemy => Vector2.Distance(transform.position, enemy.transform.position))
-            .FirstOrDefault();
-    }
-
-    public void TakeDamage(float amount)
-    {
-        currentHealth -= amount;
-
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    public void Die()
-    {
-        Destroy(gameObject);
-    }
-
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawWireSphere(transform.position, type.FireRange);
-    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
---
+++ Assets/Scripts/Turrets/Turret.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class Turret : MonoBehaviour, ITurret
+{
+    [SerializeField] protected TurretSO type;
+
+    protected List<BaseEnemy> enemiesInRange = new List<BaseEnemy>();
+    protected Quaternion defaultRotation;
+    protected float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = type.HitPoint;
+        defaultRotation = transform.rotation;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void FireAt(BaseEnemy target)
+    {
+        GameObject projectile = Instantiate(type.ProjectilePrefab, transform.position, Quaternion.identity);
+        projectile.GetComponent<Projectile>().SetValues(type.BulletSpeed, type.BulletDamage);
+        projectile.GetComponent<Projectile>().Initialize(target);
+    }
+
+    public BaseEnemy GetClosestEnemy()
+    {
+        enemiesInRange = enemiesInRange.Where(enemy => enemy != null).ToList();
+
+        if (enemiesInRange.Count == 0) return null;
+
+        Debug.Log("Enemies in range: " + enemiesInRange.Count);
+        return enemiesInRange
+            .OrderBy(enemy => Vector2.Distance(transform.position, enemy.transform.position))
+            .FirstOrDefault();
+    }
+
+    public void RotateBackToDefault()
+    {
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            defaultRotation,
+            type.RotationSpeed * Time.deltaTime
+        );
+    }
+
+    public void RotateTowards(Vector3 targetPosition)
+    {
+        Vector3 direction = targetPosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            targetRotation,
+            type.RotationSpeed * Time.deltaTime
+        );
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+}
